522fe919e2b981fae8b369115c64cffe959456ff
Victor Souza <victorsouzavieira.22@gmail.com>
2025-01-15 23:40:14 -0300

    Dbugando e simplificando o código

--- script.js
+++ script.js
@@ -3,43 +3,56 @@ let lista = document.querySelector('#lista-container');
 
 
  function criartarefa () {
-    
-        let newitem = document.createElement('li')
-        newitem.className = 'item-lista';
-        let divlist = document.createElement('div')
-        divlist.className = 'divlista'
 
+    // let divlist = document.createElement('div')
+    // divlist.className = 'divlista'
 
-        let btn = document.createElement('button')
-        btn.className = 'material-symbols-outlined';
-        btn.innerHTML = "<button onclick = excluir() class= material-symbols-outlined>close</button>"
+        let novoItem = document.createElement('li')
+        novoItem.className = 'item-lista';
+
+        let btnExcluirItem = document.createElement('button')
+        btnExcluirItem.className = 'material-symbols-outlined';
+        btnExcluirItem.innerHTML = "close"
+        console.log(btnExcluirItem)
+
  
-        btn.onclick = function(){
-            excluir(newitem)
+        btnExcluirItem.onclick = function(){
+            excluir(novoItem)
         }
 
-        let newH2 = document.createElement('h2');
+        let divH2 = document.createElement('div')
+        divH2.className = 'divH2'
+        let novoH2 = document.createElement('h2');
+        
+        let divBtns = document.createElement ('div');
+        divBtns.className = 'divbotoes'
+        
+        let checkInput = document.createElement('input')
+        checkInput.type = 'checkbox'
+        checkInput.id = 'checkbox'
+        let checkLabel = document.createElement('label')
+        checkLabel.setAttribute('for','checkbox')
 
-            let check = document.createElement('div');
-            check.className = 'customcheck';
-            let checkinput = document.createElement('input');
-            checkinput.type = 'checkbox';
-            checkinput.id = 'checkbox';
-            let label = document.createElement('label');
-            label.setAttribute('for', 'checkbox')
-            
-              
-            divlist.appendChild(newH2);
-            divlist.appendChild(btn);
+            // let checkdiv = document.createElement('div');
+            // checkdiv.className = 'customcheck';
+            // let checkinput = document.createElement('input');
+            // checkinput.type = 'checkbox';
+            // checkinput.id = 'checkbox';
+            // let label = document.createElement('label');
+            // label.setAttribute('for', 'checkbox')
+                          
+            divH2.appendChild(novoH2);
+            divBtns.appendChild(btnExcluirItem);
+            divBtns.appendChild(checkInput)
+            divBtns.appendChild(checkLabel)
+            novoItem.appendChild(divH2);
+            novoItem.appendChild(divBtns);
 
-            newitem.appendChild(divlist);
-            newitem.appendChild(check);
+            // checkdiv.appendChild(checkinput);
+            // checkdiv.appendChild(label); 
 
-            check.appendChild(checkinput);
-            check.appendChild(label);
-
-            newH2.innerHTML = input.value;
-            lista.appendChild(newitem);
+            novoH2.innerHTML = input.value;
+            lista.appendChild(novoItem);
  }
 
 
@@ -52,15 +65,11 @@ let lista = document.querySelector('#lista-container');
 
  input.addEventListener('keyup', addbutton);
 
-    
-  function tarefaconcluida () {
-    
- 
-  }
+
     
 
-    function excluir (newitem) {
-            newitem.remove();
+    function excluir (novoItem) {
+            novoItem.remove();
     }
 
     function apagartudo(){
