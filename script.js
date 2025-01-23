80969fdd24eb8ccbda721d0b3ee6ce0a0d0f8c47
Victor Souza <victorsouzavieira.22@gmail.com>
2025-01-23 16:00:32 -0300

    Revisar código

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let lista = document.querySelector('#lista-container');
 
     
         let novoItem = document.createElement('li')
-        novoItem.className = 'item-lista';
+        novoItem.className = 'item';
 
         let btnExcluirItem = document.createElement('button')
         btnExcluirItem.className = 'material-symbols-outlined';
@@ -50,6 +50,7 @@ let lista = document.querySelector('#lista-container');
         }
         })
 
+
         divEsquerda.appendChild(novoH2);
         divEsquerda.appendChild(btnExcluirItem);
 
@@ -63,14 +64,17 @@ let lista = document.querySelector('#lista-container');
         lista.appendChild(novoItem);
  }
 
- function addbutton (e) {
-    if (e.key === 'Enter') {
-        criartarefa()
-        input.value = "";
+
+ function addbutton () { 
+    if (!input.value.trim()) {
+    criartarefa()
+    alert('O campo de texto está vazio. Digite algo.');
+    input.addEventListener('keyup')
+    return;
     }
   }
-
- input.addEventListener('keyup', addbutton);
+ 
+  
 
 
     function excluir (novoItem) {
@@ -80,3 +84,34 @@ let lista = document.querySelector('#lista-container');
     function apagarTudo(){
         lista.innerHTML = " ";
     }
+
+    // const inputPesquisa = document.getElementById('search')
+
+    // inputPesquisa.addEventListener('input' ,(event)=>{
+    //     const value = event.target.value
+    // })
+    // const itens = document.querySelectorAll('#lista-container .item');
+    // itens.forEach(item =>{
+    //     console.log(formatString(item.textContent).indeOf(value) !== -1) {
+    //         item.style.display = 'flex';
+    //     } else {
+
+    //         item.style.display = 'none'
+    //     }
+    // })
+
+    function localizarItem (){
+        let barraDePesquisa = document.getElementById('search').value
+        barraDePesquisa=barraDePesquisa.toLowerCase();
+        let empty = true;
+          
+        for (i = 0; i < x.length; i++) { 
+            if (!novoItem[i].innerHTML.toLowerCase().includes(barraDePesquisa)) {
+                novoItem[i].style.display="none";
+            }
+            else {
+                novoItem[i].style.display="lista-container";
+                empty = false;
+            }
+        }
+ }
